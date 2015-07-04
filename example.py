a39afc28c7c5436ebda5f5df4322e37233c42146
Menghan <menghan412@gmail.com>
2015-07-04 12:11:44 +0800

    autopep8

--- example.py
+++ example.py
@@ -13,10 +13,10 @@ beanstalk = Beanstalk(app)
 
 @app.route('/')
 def index():
-  secs = random.randint(0, 10)
-  beanstalk.put(str(secs))
-  return "placed job that sleeps for %d seconds" % secs
+    secs = random.randint(0, 10)
+    beanstalk.put(str(secs))
+    return "placed job that sleeps for %d seconds" % secs
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
--- example_worker.py
+++ example_worker.py
@@ -7,18 +7,18 @@ import gevent
 
 class ExampleWorker(Worker):
 
-  def work(self, job):
-    self._logger.info("got job: sleep for %r seconds" % job.body)
-    gevent.sleep(int(job.body))
-    self._logger.info("finished job")
-    job.delete()
+    def work(self, job):
+        self._logger.info("got job: sleep for %r seconds" % job.body)
+        gevent.sleep(int(job.body))
+        self._logger.info("finished job")
+        job.delete()
 
 
 if __name__ == '__main__':
-  print "Spawn 5 workers"
-  workers = ExampleWorker.spawn_workers(5)
-  try:
-    while True:
-      gevent.sleep(10000)
-  except KeyboardInterrupt:
-    ExampleWorker.stop_workers(workers)
+    print "Spawn 5 workers"
+    workers = ExampleWorker.spawn_workers(5)
+    try:
+        while True:
+            gevent.sleep(10000)
+    except KeyboardInterrupt:
+        ExampleWorker.stop_workers(workers)
